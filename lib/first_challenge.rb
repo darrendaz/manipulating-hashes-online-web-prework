de3ed9faebf8c8ce73179c422277701f86b2eec0
Darren L <darren.lou@gmail.com>
2019-02-24 19:45:41 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
   }
   
   contacts["Jon Snow"][:favorite_icecream_flavors] << "mint chip"
-  contacts["Jon Snow"][:favorite_icecream_flavors].pop("strawberry")
+  contacts["Freddy Mercury"][:favorite_icecream_flavors].pop("strawberry")
  
   puts contacts 
 
