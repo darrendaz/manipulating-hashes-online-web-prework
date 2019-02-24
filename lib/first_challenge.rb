6521c105da1ca677b8af4288d3b9b3505e070138
Darren L <darren.lou@gmail.com>
2019-02-24 19:42:27 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,10 +13,9 @@ def first_challenge
     }
   }
 
-  
+contacts["Jon Snow"][:favorite_icecream_flavors] << "mint chip"
+ 
+puts contacts 
 
-
-  #remember to return your newly altered contacts hash!
-  contacts
 end
 
