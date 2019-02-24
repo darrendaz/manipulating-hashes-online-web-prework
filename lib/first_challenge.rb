bdd21c80dde3d43a961d5646e81aa047311ef9cf
Darren L <darren.lou@gmail.com>
2019-02-24 19:46:54 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
   }
   
   contacts["Jon Snow"][:favorite_icecream_flavors] << "mint chip"
-  contacts["Freddy Mercury"][:favorite_icecream_flavors].pop("strawberry")
+  contacts["Freddy Mercury"][:favorite_icecream_flavors].delete("strawberry")
  
   puts contacts 
 
